217590acfeb52a790b4f61bf6395475783037de4
JustAdjika <fenixxfns@gmail.com>
2025-09-20 19:04:54 +0300

    feat: добавил лоадер

---
+++ src/components/loader.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style/loader.css'
+
+const Loader = () => {
+    return (
+        <div style={{ width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <span className="loader"></span>
+        </div>
+    );
+};
+
+export default Loader;
