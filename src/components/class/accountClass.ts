ac7dc6380b6909e30ebf93a28c345d6592f2e703
JustAdjika <fenixxfns@gmail.com>
2025-09-21 23:19:36 +0300

    feat: добавил класс аккаунта

---
+++ src/components/class/accountClass.ts
@@ -0,0 +1,133 @@
+import Cookies from 'js-cookie'
+
+import * as Types from '../../../module/types/types.ts'
+import { request } from '../../serverRequest.ts'
+import { getUser } from '../../getUser.ts'
+import api from '../../axiosConfig.ts'
+
+type extendedAccount = Types.Account & { role: Types.permission | 'USER' | 'VOLUNTEER' } 
+
+export class Account {
+    data: extendedAccount;
+
+    private constructor(data: extendedAccount) {
+        this.data = data;
+    }
+
+    static async create(setErrorMessage: (message: string | null) => void): Promise<Account> {
+        const user = await getUser({ setErrorMessage }) as Types.Account
+        
+        const isVolunteer = await request({ method: 'GET', route: '/perms/is/AV_VOLUNTEER', loadQuery: { userId: user.id } })
+        const isCoordinator = await request({ method: 'GET', route: '/perms/is/COORDINATOR', loadQuery: { userId: user.id } })
+        const isAdmin = await request({ method: 'GET', route: '/perms/is/ADMIN', loadQuery: { userId: user.id } })
+
+        const extendedAccount: extendedAccount = { ...user, role: isAdmin ? 'ADMIN' : isCoordinator ? 'COORDINATOR' : isVolunteer ? 'VOLUNTEER' : 'USER' }
+
+        return new Account(extendedAccount);
+    }
+
+    async updateContactInfo({ contactWhatsapp, contactKaspi }: { contactWhatsapp: string | null, contactKaspi: string | null }) {
+        const session = Cookies.get("session")
+
+        if(session) {
+            const parsedSession = JSON.parse(session) as Types.Session
+
+            const res = await request({ method: 'PUT', route: '/account/info/contact/edit', loadData: {
+                userId: this.data.id,
+                sessionId: parsedSession.id,
+                sessionKey: parsedSession.key,
+                contactInfo: {
+                    contactKaspi,
+                    contactWhatsapp
+                }
+            } })
+
+            return res
+        }
+    }
+
+    async updateEmailgetToken(email: string) {
+        const session = Cookies.get("session")
+
+        if(session) {
+            const parsedSession = JSON.parse(session) as Types.Session
+
+            const res = await request({ method: 'POST', route: '/account/emailChange', loadData: {
+                userId: this.data.id,
+                sessionId: parsedSession.id,
+                sessionKey: parsedSession.key,
+                newEmail: email
+            } })
+
+            if(res.status === 200) return res as { status: Types.statusCode, container: { confirmToken: string } }
+            else return res
+        }
+    }
+
+    async updateEmailconfirm({ code, token }: { code: string, token: string }) {
+        const session = Cookies.get("session")
+
+        if(session) {
+            const parsedSession = JSON.parse(session) as Types.Session
+
+            const res = await request({ method: 'POST', route: `/account/emailConfirm/${token}`, loadData: {
+                code,
+                sessionId: parsedSession.id,
+                sessionKey: parsedSession.key
+            } })
+
+            if(res.status === 200) return res as { status: Types.statusCode, container: { updateData: Types.Account } }
+            else return res
+        }
+    }
+
+    async uploadIdCard(file: File) {
+        const session = Cookies.get("session")
+
+        if(session) {
+            const parsedSession = JSON.parse(session) as Types.Session
+
+            const formData = new FormData()
+
+            formData.append("idcard", file)
+            formData.append("sessionId", String(parsedSession.id))
+            formData.append("sessionKey", parsedSession.key)
+
+            const res = await api.post(`/account/idCard/upload`, formData, { headers: { "Content-Type": "multipart/form-data" } })
+
+            return res
+        }
+    }
+
+    async getEquip() {
+        const session = Cookies.get("session")
+
+        if(session) {
+            const parsedSession = JSON.parse(session) as Types.Session
+
+            const res = await request({ method: 'POST', route: `/account/equipment/qrgenerate/get`, loadData: {
+                sessionId: parsedSession.id,
+                sessionKey: parsedSession.key
+            } })
+
+            if(res.status === 200) return res as { status: Types.statusCode, container: { qrId: string } }
+            else return res
+        }
+    }
+
+    async returnEquip() {
+        const session = Cookies.get("session")
+
+        if(session) {
+            const parsedSession = JSON.parse(session) as Types.Session
+
+            const res = await request({ method: 'POST', route: `/account/equipment/qrgenerate/return`, loadData: {
+                sessionId: parsedSession.id,
+                sessionKey: parsedSession.key
+            } })
+
+            if(res.status === 200) return res as { status: Types.statusCode, container: { qrId: string } }
+            else return res
+        }
+    }
+}
