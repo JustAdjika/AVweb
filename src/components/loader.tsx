bc5d84ccf10b48584282cfe03fec91042a8735d8
JustAdjika <fenixxfns@gmail.com>
2025-09-22 20:14:08 +0300

    feat: добавил возможность стилизовать лоадер

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,8 +1,8 @@
 import './style/loader.css'
 
-const Loader = () => {
+const Loader = ({ style }: { style?: object }) => {
     return (
-        <div style={{ width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <div style={{ width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', ...style }}>
             <span className="loader"></span>
         </div>
     );
