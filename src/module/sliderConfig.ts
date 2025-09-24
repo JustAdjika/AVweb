eadcfa04d8c4736dc0963fe467f9d0dbd9ed2bb6
JustAdjika <fenixxfns@gmail.com>
2025-09-24 16:13:38 +0300

    refactor: добавил конфиг для слайдера

---
+++ src/module/sliderConfig.ts
@@ -0,0 +1,51 @@
+interface slide {
+    title: string,
+    date: string,
+    staffs: number,
+    img: string
+}
+
+export const slides: slide[] = [
+    {
+        title: '50CENT',
+        date: '28.11.23',
+        staffs: 100,
+        img: './src/assets/img/event_50cent.png'
+    },
+    {
+        title: 'ДЖЕННИФЕР ЛОПЕС',
+        date: '10.08.25',
+        staffs: 100,
+        img: './src/assets/img/event_jenifer.png'
+    },
+    {
+        title: 'ЕГОР КРИД',
+        date: '27.03.24',
+        staffs: 100,
+        img: './src/assets/img/event_krid.png'
+    },
+    {
+        title: 'SCORPIONS',
+        date: '28.05.24',
+        staffs: 100,
+        img: './src/assets/img/event_scorpions.png'
+    },
+    {
+        title: 'MACAN',
+        date: '28.11.24',
+        staffs: 100,
+        img: './src/assets/img/event_macan.png'
+    },
+    {
+        title: 'AKON',
+        date: '15.12.24',
+        staffs: 100,
+        img: './src/assets/img/event_akon.png'
+    },
+    {
+        title: 'SADRADDIN',
+        date: '15.09.24',
+        staffs: 300,
+        img: './src/assets/img/event_sadraddin.png'
+    },
+]
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -7,6 +7,7 @@ import { errorLogger } from '../module/errorLogger.ts';
 import { request } from '../module/serverRequest.ts';
 import PhoneInput from '../components/phoneInput';
 import Loader from '../components/loader.tsx';
+import { slides } from '../module/sliderConfig.ts';
 
 import './style/main.css'
 
@@ -88,58 +89,6 @@ export const Main = ({ setErrorMessage }: Props) => {
             setInviteViewState(true)
         }
     }, [isInviteView])
-
-    interface slide {
-        title: string,
-        date: string,
-        staffs: number,
-        img: string
-    }
-
-    const slides2: slide[] = [
-        {
-            title: '50CENT',
-            date: '28.11.23',
-            staffs: 100,
-            img: './src/assets/img/event_50cent.png'
-        },
-        {
-            title: 'ДЖЕННИФЕР ЛОПЕС',
-            date: '10.08.25',
-            staffs: 100,
-            img: './src/assets/img/event_jenifer.png'
-        },
-        {
-            title: 'ЕГОР КРИД',
-            date: '27.03.24',
-            staffs: 100,
-            img: './src/assets/img/event_krid.png'
-        },
-        {
-            title: 'SCORPIONS',
-            date: '28.05.24',
-            staffs: 100,
-            img: './src/assets/img/event_scorpions.png'
-        },
-        {
-            title: 'MACAN',
-            date: '28.11.24',
-            staffs: 100,
-            img: './src/assets/img/event_macan.png'
-        },
-        {
-            title: 'AKON',
-            date: '15.12.24',
-            staffs: 100,
-            img: './src/assets/img/event_akon.png'
-        },
-        {
-            title: 'SADRADDIN',
-            date: '15.09.24',
-            staffs: 300,
-            img: './src/assets/img/event_sadraddin.png'
-        },
-    ]
 
 
 
@@ -232,7 +181,7 @@ export const Main = ({ setErrorMessage }: Props) => {
                     <div className='main-projects-title-decorative-line'/>
                 </div>
                 <Slider
-                    slides={slides2.map((item) => (
+                    slides={slides.map((item) => (
                         <SlideComponent info={item} />
                     ))}
                 />
