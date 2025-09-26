e202731b125acdb41dde4a3ac79901e35c933316
JustAdjika <fenixxfns@gmail.com>
2025-09-26 07:07:39 +0300

    chore: добавил в конфиг домен для ngrok

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import svgr from '@svgr/rollup'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    host: true,       // позволяет подключаться по локальному IP (например, 192.168.1.10)
+    port: 5173,
+    allowedHosts: [
+      'remy-evangelistic-chery.ngrok-free.dev'
+    ]
+  }
 })
